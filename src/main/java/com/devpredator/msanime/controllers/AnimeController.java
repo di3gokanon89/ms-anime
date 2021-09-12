74c51037d8c9bfa60a7c711efb626dc1d10f7446
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2021-09-11 20:53:13 -0500

    Se realizan la integración de Spring Security y Swagger para documentar el microservicio de animes

--- src/main/java/com/devpredator/msanime/controllers/AnimeController.java
+++ src/main/java/com/devpredator/msanime/controllers/AnimeController.java
@@ -23,8 +23,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devpredator.MsAnimeApplication;
 import com.devpredator.msentity.entity.Anime;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import com.devpredator.msanime.service.AnimeService;
 
 /**
@@ -35,6 +41,7 @@ import com.devpredator.msanime.service.AnimeService;
  * @version v1.0 21/07/2021
  *
  */
+@Api(tags = {"ANIME'S MICROSERVICE"})
 @RestController
 @RequestMapping("/anime")
 public class AnimeController {
@@ -54,6 +61,13 @@ public class AnimeController {
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(AnimeController.class); 
 	
+	@ApiOperation(value = "FIND ANIMES", notes = "Get the list of animes", response = List.class)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Animes founded successfully!"),
+		@ApiResponse(code = 401, message = "You don't have access to this endpoint!"),
+		@ApiResponse(code = 403, message = "Anime's forbidden!"),
+		@ApiResponse(code = 404, message = "Animes's resource not founded!")
+	})
 	@GetMapping("findAnimes")
 	public ResponseEntity<?> findAnimes() {
 		LOGGER.info("Run findAnimes service.");
@@ -72,9 +86,18 @@ public class AnimeController {
 		
 		return ResponseEntity.ok(animes);
 	}
-	
+
+	@ApiOperation(value = "FIND ANIME BY ID", notes = "Get an anime by its id", response = Anime.class)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Anime founded successfully!"),
+		@ApiResponse(code = 401, message = "You don't have access to this endpoint!"),
+		@ApiResponse(code = 403, message = "Anime's forbidden!"),
+		@ApiResponse(code = 404, message = "Animes's resource not founded!")
+	})
 	@GetMapping("findAnimeById/{id}")
-	public ResponseEntity<?> findAnimeById(@PathVariable("id") Long idAnime) {
+	public ResponseEntity<?> findAnimeById(
+			@ApiParam(value = "The id of the anime to find", name = "id", required = true, example = "Example: 10") 
+			@PathVariable("id") Long idAnime) {
 		
 		Optional<Anime> animeOptional = this.animeServiceImpl.findAnimeById(idAnime);
 		
@@ -85,23 +108,53 @@ public class AnimeController {
 		return ResponseEntity.ok(animeOptional.get());
 	}
 	
+	@ApiOperation(value = "SAVE ANIME", notes = "Store a new anime in the database", response = Anime.class)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Anime request successfully!"),
+		@ApiResponse(code = 201, message = "Anime created successfully!"),
+		@ApiResponse(code = 401, message = "You don't have access to this endpoint!"),
+		@ApiResponse(code = 403, message = "Anime's forbidden!"),
+		@ApiResponse(code = 404, message = "Animes's resource not founded!")
+	})
 	@PostMapping("saveAnime")
-	public ResponseEntity<?> saveAnime(@Valid @RequestBody Anime anime) {
+	public ResponseEntity<?> saveAnime(
+			@ApiParam(value = "The object of the anime to store", name = "anime", required = true) 
+			@Valid @RequestBody Anime anime) {
 		
 		Anime animeSaved = this.animeServiceImpl.saveAnime(anime);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(animeSaved);
 	}
 	
+	@ApiOperation(value = "UPDATE ANIME", notes = "Update an anime in the database", response = Anime.class)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Anime request successfully!"),
+		@ApiResponse(code = 201, message = "Anime updated successfully!"),
+		@ApiResponse(code = 401, message = "You don't have access to this endpoint!"),
+		@ApiResponse(code = 403, message = "Anime's forbidden!"),
+		@ApiResponse(code = 404, message = "Animes's resource not founded!")
+	})
 	@PutMapping("updateAnime")
-	public ResponseEntity<?> updateAnime(@RequestBody Anime anime) {
+	public ResponseEntity<?> updateAnime(
+			@ApiParam(value = "The object of the anime to update", name = "anime", required = true) 
+			@Valid @RequestBody Anime anime) {
 		Anime animeUpdated = this.animeServiceImpl.saveAnime(anime);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(animeUpdated);
 	}
 	
+	@ApiOperation(value = "DELETE ANIME", notes = "Delete an anime in the database", response = ResponseEntity.class)
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Anime deleted successfully!"),
+		@ApiResponse(code = 204, message = "No content for this anime!"),
+		@ApiResponse(code = 401, message = "You don't have access to this endpoint!"),
+		@ApiResponse(code = 403, message = "Anime's forbidden!"),
+		@ApiResponse(code = 404, message = "Animes's resource not founded!")
+	})
 	@DeleteMapping("deleteAnimeById/{id}")
-	public ResponseEntity<?> deleteAnimeById(@PathVariable("id") Long idAnime) {
+	public ResponseEntity<?> deleteAnimeById(
+			@ApiParam(value = "The id of the anime to delete", name = "id", required = true, example = "Example: 1") 
+			@PathVariable("id") Long idAnime) {
 		
 		this.animeServiceImpl.deleteAnimeById(idAnime);
 		
