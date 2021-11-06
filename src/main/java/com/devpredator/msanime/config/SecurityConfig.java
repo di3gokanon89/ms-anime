1d6f585ce09291a832b2bd4fff15aadff5bd11c1
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2021-11-06 17:57:55 -0600

    Proyecto Anime - Mockito Section - Create Unit test with mockito to test the characters business logic

--- src/main/java/com/devpredator/msanime/config/SecurityConfig.java
+++ src/main/java/com/devpredator/msanime/config/SecurityConfig.java
@@ -6,6 +6,7 @@ package com.devpredator.msanime.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  *
  */
 @Configuration
+@Profile(value = {"dev", "prod", "uat"})
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
--- src/test/java/com/devpredator/msanime/service/test/AnimeServiceTest.java
+++ src/test/java/com/devpredator/msanime/service/test/AnimeServiceTest.java
@@ -4,7 +4,10 @@
 package com.devpredator.msanime.service.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,11 +21,20 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.devpredator.msanime.exceptions.AnimeNullException;
+import com.devpredator.msanime.repository.AnimeRepository;
 import com.devpredator.msanime.service.AnimeService;
+import com.devpredator.msanime.service.impl.AnimeServiceImpl;
 import com.devpredator.msentity.entity.Anime;
 
 /**
@@ -33,10 +45,16 @@ import com.devpredator.msentity.entity.Anime;
  * Unit test for Animes Service Business.
  */
 @SpringBootTest
+@ActiveProfiles(value = {"test"})
+@ExtendWith(MockitoExtension.class)
 class AnimeServiceTest {
 
-	@Autowired
-	private AnimeService animeServiceImpl;
+	@Mock
+	private AnimeRepository animeRepository;
+	
+	//@Autowired
+	@InjectMocks
+	private AnimeServiceImpl animeServiceImpl;
 	/**
 	 * Log the details of a unit test.
 	 */
@@ -59,6 +77,12 @@ class AnimeServiceTest {
 		
 		this.testReporter.publishEntry("DISPLAY NAME: " + this.testInfo.getDisplayName());
 		this.testReporter.publishEntry("TEST METHOD: " + this.testInfo.getTestMethod());
+		
+		//::::: MANUAL CONFIGURATION WITH MOCKS :::::
+		//animeServiceImpl = mock(AnimeService.class);
+		
+		//::::: CONFIGURATION WITH ANNOTATIONS ::::::
+		//MockitoAnnotations.openMocks(this);
 	}
 	
 	@DisplayName(value = "NESTED ANIMES TO FIND")
@@ -70,11 +94,27 @@ class AnimeServiceTest {
 		@DisplayName(value = "FIND ALL ANIMES")
 		@Test
 		void testFindAnimes() {
+			
+			//:::::::::::::::::: GIVEN :::::::::::::::::::::
+			
+			//Defines the list of animes to test a simulated data from the database.
+			Iterable<Anime> animesIterable = List.of(
+					new Anime(1L, "Dragon Ball Z", 1986, LocalDateTime.of(2021, 2, 20, 02, 24, 44)),
+					new Anime(2L, "Hellsing", 2004, LocalDateTime.of(2021, 2, 20, 05, 24, 44))
+			);
+			
+			// :::::::::::::::: WHEN ::::::::::::::::::::::
+			
+			when(animeRepository.findAll()).thenReturn(animesIterable);
+			
+			// :::::::::::::::: THEN ::::::::::::::::::::::
 			List<Anime> animes = animeServiceImpl.findAnimes();
 			
 			assertNotNull(animes);
 			assertTrue(animes.size() > 0);
 			testReporter.publishEntry("List of animes founded: " + animes.size());
+			
+			verify(animeRepository).findAll();
 		}
 
 		/**
@@ -84,10 +124,22 @@ class AnimeServiceTest {
 		@Test
 		void testFindAnimeById() {
 			
-			Optional<Anime> anime = animeServiceImpl.findAnimeById(1L);
+			//::::::::::::::::: GIVEN :::::::::::::::::::::
 			
-			assertNotNull(anime.get());
-			assertTrue(anime.get().getNombre().equals("Shingeky no Kyojin"));
+			Long id = 1L;
+			Anime anime = new Anime(1L, "Demon Slayer", 2019, LocalDateTime.of(2021, 10, 11, 12, 12, 12));
+			
+			//::::::::::::::::: WHEN ::::::::::::::::::::::
+			
+			when(animeRepository.findById(id)).thenReturn(Optional.of(anime));
+			
+			//::::::::::::::::: THEN ::::::::::::::::::::::
+			Optional<Anime> animeOptional = animeServiceImpl.findAnimeById(id);
+			
+			assertNotNull(animeOptional.get());
+			assertTrue(animeOptional.get().getNombre().equals("Demon Slayer"));
+			
+			verify(animeRepository).findById(id);
 		}	
 	}
 	
@@ -99,19 +151,32 @@ class AnimeServiceTest {
 		@DisplayName(value = "STORE AN ANIME IS OK")
 		@Test
 		void testSaveAnimeIsOk() {
+			
+			//::::::::::::::: GIVEN ::::::::::::::::::::::
+			
 			Anime anime = new Anime();
+			anime.setId(10L);
 			anime.setNombre("Ragnarok");
 			anime.setAnio(2021);
+			anime.setFechaCreacion(LocalDateTime.of(2021, 9, 10, 10, 10, 10));
 			
+			//::::::::::::::: WHEN ::::::::::::::::::::::::
+			
+			when(animeRepository.save(any(Anime.class))).thenReturn(anime);
+			
+			//::::::::::::::: THEN :::::::::::::::::::::::::
 			Anime animeStored = animeServiceImpl.saveAnime(anime);
 			
 			assertNotNull(animeStored);
 			assertNotNull(animeStored.getId());
 			
+			assertEquals(10L, animeStored.getId());
 			assertEquals("Ragnarok", animeStored.getNombre());
 			assertEquals(2021, animeStored.getAnio());
 			
 			testReporter.publishEntry("Message", "Anime saved successfully");
+			
+			verify(animeRepository).save(any(Anime.class));
 		}
 
 		/**
@@ -133,13 +198,23 @@ class AnimeServiceTest {
 	@DisplayName(value = "DELETE AN ANIME")
 	@Test
 	void testDeleteAnimeById() {
-		animeServiceImpl.deleteAnimeById(34L);
+				
+		animeServiceImpl.deleteAnimeById(anyLong());
+		
+		//:::::::::::::::: WHEN :::::::::::::::::::::::::::
+		
+		when(animeRepository.findById(anyLong())).thenReturn(Optional.empty());
+		
+		//:::::::::::::::: THEN :::::::::::::::::::::::::::
 		
 		Optional<Anime> animeNotFound = animeServiceImpl.findAnimeById(34L);
 
 		testReporter.publishEntry("Message", "Anime deleted successfully");
 
 		assertTrue(animeNotFound.isEmpty());
+		
+		verify(animeRepository).findById(anyLong());
+		verify(animeRepository).deleteById(anyLong());
 		
 	}
 	
